45bd7685f474c2c168061c7aa7958302abe46f10
Johannes Ptaszyk <johannes@ptaszyk.de>
2023-11-01 14:35:23 +0100

    FIX sonar paths

--- build-logic/convention/src/main/kotlin/configurations/SonarConfiguration.kt
+++ build-logic/convention/src/main/kotlin/configurations/SonarConfiguration.kt
@@ -10,11 +10,12 @@ fun Project.applySonar() {
     pluginManager.apply(SonarQubePlugin::class)
     configure<SonarExtension> {
         properties {
+            property("sonar.sourceEncoding", "UTF-8")
             property("sonar.projectKey", "JohannesPtaszyk_Abonity")
             property("sonar.organization", "johannesptaszyk")
             property("sonar.host.url", "https://sonarcloud.io")
-            property("sonar.kotlin.detekt.reportPaths", layout.buildDirectory.file("reports/detekt/merge.xml"))
-            property("sonar.coverage.jacoco.xmlReportPaths", layout.buildDirectory.file("reports/kover/report.xml"))
+            property("sonar.kotlin.detekt.reportPaths", layout.buildDirectory.file("reports/detekt/merge.xml").get().asFile.path)
+            property("sonar.coverage.jacoco.xmlReportPaths", layout.buildDirectory.file("reports/kover/report.xml").get().asFile.path)
         }
     }
 }
