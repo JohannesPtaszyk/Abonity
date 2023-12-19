ba3876d74453ca47eae449aa4b0d31d9dde9184a
Johannes Ptaszyk <johannes@ptaszyk.de>
2023-12-19 21:37:21 +0100

    ADD test for OverviewViewModel argument handling

--- feature/subscription/src/test/kotlin/dev/pott/abonity/feature/subscription/overview/OverviewViewModelTest.kt
+++ feature/subscription/src/test/kotlin/dev/pott/abonity/feature/subscription/overview/OverviewViewModelTest.kt
@@ -29,15 +29,13 @@ class OverviewViewModelTest {
     @Test
     fun `GIVEN local subscriptions WHEN initializing THEN return list of overview items AND period prices`() {
         runTest {
-            val subscription =
-                createTestSubscription(
-                    paymentInfo =
-                    PaymentInfo(
-                        price = Price(1.0, Currency.getInstance("EUR")),
-                        firstPayment = LocalDate(2021, 1, 1),
-                        type = PaymentType.Periodic(2, PaymentPeriod.DAYS),
-                    ),
-                )
+            val subscription = createTestSubscription(
+                paymentInfo = PaymentInfo(
+                    price = Price(1.0, Currency.getInstance("EUR")),
+                    firstPayment = LocalDate(2021, 1, 1),
+                    type = PaymentType.Periodic(2, PaymentPeriod.DAYS),
+                ),
+            )
             val localSubscriptionFlow = flowOf(listOf(subscription))
             val repository = FakeSubscriptionRepository(localSubscriptionFlow)
             val clock = FakeClock()
@@ -69,17 +67,47 @@ class OverviewViewModelTest {
     }
 
     @Test
+    fun `GIVEN id argument WHEN initializing THEN id is preselected`() {
+        runTest {
+            val subscription = createTestSubscription(
+                paymentInfo = PaymentInfo(
+                    price = Price(1.0, Currency.getInstance("EUR")),
+                    firstPayment = LocalDate(2021, 1, 1),
+                    type = PaymentType.Periodic(2, PaymentPeriod.DAYS),
+                ),
+            )
+            val localSubscriptionFlow = flowOf(listOf(subscription))
+            val repository = FakeSubscriptionRepository(localSubscriptionFlow)
+            val clock = FakeClock()
+            val dateCalculator = PaymentDateCalculator(clock)
+            val infoCalculator = PaymentInfoCalculator(dateCalculator, clock)
+            val useCase = GetSubscriptionsWithPeriodPrice(
+                repository,
+                infoCalculator,
+                dateCalculator,
+            )
+            val savedStateHandle = SavedStateHandle(
+                mapOf(OverviewScreenDestination.Args.DETAIL_ID_KEY to subscription.id.value),
+            )
+            val tested = OverviewViewModel(savedStateHandle, useCase, infoCalculator)
+
+            tested.state.test {
+                assertThat(awaitItem()).isEqualTo(OverviewState())
+                assertThat(awaitItem().detailId).isEqualTo(subscription.id)
+            }
+        }
+    }
+
+    @Test
     fun `GIVEN initialized vm WHEN openDetails THEN state contains detail id`() {
         runTest {
-            val subscription =
-                createTestSubscription(
-                    paymentInfo =
-                    PaymentInfo(
-                        price = Price(1.0, Currency.getInstance("EUR")),
-                        firstPayment = LocalDate(2021, 1, 1),
-                        type = PaymentType.Periodic(2, PaymentPeriod.DAYS),
-                    ),
-                )
+            val subscription = createTestSubscription(
+                paymentInfo = PaymentInfo(
+                    price = Price(1.0, Currency.getInstance("EUR")),
+                    firstPayment = LocalDate(2021, 1, 1),
+                    type = PaymentType.Periodic(2, PaymentPeriod.DAYS),
+                ),
+            )
             val localSubscriptionFlow = flowOf(listOf(subscription))
             val repository = FakeSubscriptionRepository(localSubscriptionFlow)
             val clock = FakeClock()
@@ -116,15 +144,13 @@ class OverviewViewModelTest {
     @Test
     fun `GIVEN initialized vm WHEN openDetails AND consumeDetails THEN state contains no id`() {
         runTest {
-            val subscription =
-                createTestSubscription(
-                    paymentInfo =
-                    PaymentInfo(
-                        price = Price(1.0, Currency.getInstance("EUR")),
-                        firstPayment = LocalDate(2021, 1, 1),
-                        type = PaymentType.Periodic(2, PaymentPeriod.DAYS),
-                    ),
-                )
+            val subscription = createTestSubscription(
+                paymentInfo = PaymentInfo(
+                    price = Price(1.0, Currency.getInstance("EUR")),
+                    firstPayment = LocalDate(2021, 1, 1),
+                    type = PaymentType.Periodic(2, PaymentPeriod.DAYS),
+                ),
+            )
             val localSubscriptionFlow = flowOf(listOf(subscription))
             val repository = FakeSubscriptionRepository(localSubscriptionFlow)
             val clock = FakeClock()
