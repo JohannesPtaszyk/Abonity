fc43c8921f2b2953851e5e5d60d11575dd5e72da
Johannes Ptaszyk <johannes@ptaszyk.de>
2024-07-29 21:21:33 +0200

    ADD filter itemss for categories

--- core/entity/src/main/kotlin/dev/pott/abonity/core/entity/subscription/SubscriptionFilterItem.kt
+++ core/entity/src/main/kotlin/dev/pott/abonity/core/entity/subscription/SubscriptionFilterItem.kt
@@ -5,4 +5,7 @@ sealed interface SubscriptionFilterItem {
     data class Currency(val price: Price) : SubscriptionFilterItem
 
     data class CurrentPeriod(val period: PaymentPeriod) : SubscriptionFilterItem
+
+    data class Category(val category: dev.pott.abonity.core.entity.subscription.Category) :
+        SubscriptionFilterItem
 }
