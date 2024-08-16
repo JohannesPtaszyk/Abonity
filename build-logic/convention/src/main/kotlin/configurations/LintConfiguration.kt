eb60c2fb7be921d034064930ee32f1867e43ad2a
Johannes Ptaszyk <johannes@ptaszyk.de>
2024-08-16 22:48:16 +0200

    ADD security lint rules

--- build-logic/convention/src/main/kotlin/configurations/LintConfiguration.kt
+++ build-logic/convention/src/main/kotlin/configurations/LintConfiguration.kt
@@ -1,12 +1,18 @@
 package configurations
 
 import com.android.build.api.dsl.Lint
+import extensions.libs
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 fun Project.configureLint(lint: Lint) {
     with(lint) {
         xmlReport = true
         checkDependencies = true
         xmlOutput = layout.buildDirectory.file("reports/lint/result.xml").get().asFile
+    }
+
+    dependencies {
+        add("lintChecks", libs.findLibrary("android.lint.security").get())
     }
 }
