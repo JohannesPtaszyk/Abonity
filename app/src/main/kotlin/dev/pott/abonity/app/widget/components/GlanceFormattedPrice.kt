1cd8445527318a95f951822bca6c3e9179fe9248
Johannes Ptaszyk <johannes@ptaszyk.de>
2025-07-24 16:12:50 +0200

    fix: glance has no support for LocalConfiguration

--- app/src/main/kotlin/dev/pott/abonity/app/widget/components/GlanceFormattedPrice.kt
+++ app/src/main/kotlin/dev/pott/abonity/app/widget/components/GlanceFormattedPrice.kt
@@ -1,10 +1,11 @@
 package dev.pott.abonity.app.widget.components
 
+import android.annotation.SuppressLint
 import android.os.Build
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.glance.GlanceModifier
+import androidx.glance.LocalContext
 import androidx.glance.unit.ColorProvider
 import dev.pott.abonity.app.widget.theme.LocalGlanceContentColor
 import dev.pott.abonity.app.widget.theme.LocalGlanceTextStyle
@@ -27,9 +28,10 @@ fun GlanceFormattedPrice(
     GlanceText(formatted, modifier, style, color, maxLines)
 }
 
+@SuppressLint("LocalContextConfigurationRead")
 @Composable
 private fun rememberFormattedPrice(value: Double, currency: Currency): String {
-    val locale = LocalConfiguration.current.let {
+    val locale = LocalContext.current.resources.configuration.let {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             it.locales[0]
         } else {
