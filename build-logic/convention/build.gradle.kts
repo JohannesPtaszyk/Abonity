2f094bb38ae0211188a301a76ccc25e34471afe8
Johannes Ptaszyk <johannes@ptaszyk.de>
2025-08-09 23:06:59 +0200

    feat: bump build-logic jvm version to 21

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 group = "dev.pott.buildlogic"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
